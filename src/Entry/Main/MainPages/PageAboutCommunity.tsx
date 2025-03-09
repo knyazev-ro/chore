f8425a2d0c1c7a0a2bb47e3201deb487d475c721
knyazevseven@gmail.com <knyazevseven@gmail.com>
2025-03-10 00:45:50 +0300

    sm

--- src/Entry/Main/MainPages/PageAboutCommunity.tsx
+++ src/Entry/Main/MainPages/PageAboutCommunity.tsx
@@ -3,23 +3,15 @@ export default function PageAboutCommunity() {
       <div className="bg-yellow-500 w-full h-full text-black p-10 comfortaa flex items-center justify-center">
         <div className="max-w-3xl p-6 text-center">
           <h1 className="text-4xl font-bold uppercase tracking-wide">О приложении</h1>
-          <p className="mt-6 text-lg opacity-80 leading-relaxed">
+          <p className="mt-6 lg:text-lg sm:text-xs opacity-80 leading-relaxed">
             Chore Community — это универсальная платформа для управления задачами, 
             которая объединяет в себе все необходимые инструменты.
           </p>
-          <p className="mt-4 text-lg opacity-80 leading-relaxed">
+          <p className="mt-4 lg:text-lg sm:text-sm opacity-80 leading-relaxed">
             Мы предлагаем удобную доску задач в стиле Jira, чтобы вы могли легко 
             организовывать рабочие процессы.
           </p>
-          <p className="mt-4 text-lg opacity-80 leading-relaxed">
-            Интерактивный календарь помогает планировать дедлайны, а встроенный 
-            трекинг задач фиксирует прогресс.
-          </p>
-          <p className="mt-4 text-lg opacity-80 leading-relaxed">
-            Уведомления не дадут вам пропустить важные события, а встроенный мессенджер 
-            обеспечивает мгновенную коммуникацию с командой.
-          </p>
-          <p className="mt-4 text-lg opacity-80 leading-relaxed">
+          <p className="mt-4 lg:text-lg sm:text-sm opacity-80 leading-relaxed">
             Простота интерфейса делает вход в систему легким даже для новичков, а 
             мощный функционал открывает безграничные возможности для продуктивной работы.
           </p>
--- src/Entry/Main/MainPages/PageOne.tsx
+++ src/Entry/Main/MainPages/PageOne.tsx
@@ -3,7 +3,7 @@ export default function PageOne() {
       <div className="bg-blue-500 w-full h-full text-white p-10 comfortaa flex items-center justify-center">
         <div className="max-w-4xl p-6 text-center">
           <h1 className="text-5xl font-bold uppercase tracking-wide">Chore Community</h1>
-          <p className="mt-6 text-xl opacity-80 leading-relaxed">
+          <p className="mt-6 lg:text-xl sm:text-sm opacity-80 leading-relaxed">
             Мы верим, что управление задачами должно быть простым. Никакой сложности, никаких лишних шагов — только 
             удобные инструменты, которые работают на вас. Создавайте, организовывайте и достигайте целей без лишних препятствий. 
             Минимум усилий. Максимум результата.
