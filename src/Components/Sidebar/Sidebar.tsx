e4e493955c72fc7393c414c93972d985af975b23
knyazevseven@gmail.com <knyazevseven@gmail.com>
2025-03-09 13:34:22 +0300

    feat: add sidebar

---
+++ src/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,63 @@
+import { ChevronDoubleRightIcon } from "@heroicons/react/24/outline";
+
+export default function Sidebar() {
+  const menuItems = [
+    {
+      id: 1,
+      title: "Trello",
+      route: "",
+    },
+    {
+      id: 2,
+      title: "Messages",
+      route: "",
+    },
+    {
+      id: 3,
+      title: "Notifications",
+      route: "",
+    },
+    {
+      id: 4,
+      title: "Calendar",
+      route: "",
+    },
+    {
+      id: 5,
+      title: "Company",
+      route: "",
+    },
+    {
+      id: 5,
+      title: "News",
+      route: "",
+    },
+    {
+      id: 5,
+      title: "Subscription",
+      route: "",
+    },
+  ];
+
+  return (
+    <div className="comfortaa flex-col flex bg-stone-950 w-64 h-full">
+      <div className="flex text-stone-100 flex-col text-md py-6 px-5 h-full justify-center">
+        {menuItems.map((e) => (
+          <>
+            <div
+              key={e.id}
+              className="relative flex items-center justify-between group transition-all duration-500 ease-in-out hover:bg-stone-900 py-4 px-3 overflow-hidden"
+            >
+              <div className="z-10">{e.title}</div>
+              <ChevronDoubleRightIcon
+                className="group-hover:opacity-100 transition-all duration-200 ease-in-out opacity-0 w-4"
+                color="white"
+              />
+              <div className="absolute w-40 h-40 rounded-full group-active:bg-stone-700/80 group-active:animate-ping translate-x-5"></div>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/Entry/Main/MainPage.tsx
+++ src/Entry/Main/MainPage.tsx
@@ -2,22 +2,35 @@ import { StarIcon } from "@heroicons/react/24/outline";
 import Logo from "../../assets/Logo";
 import PageOne from "./MainPages/PageOne";
 import PageAboutCommunity from "./MainPages/PageAboutCommunity";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import { useState } from "react";
 
 export default function MainPage() {
-  const avialablePages = [PageOne, PageAboutCommunity,];
+  const [open, setOpen] = useState(true);
+  const avialablePages = [PageOne, PageAboutCommunity];
 
   return (
     <div className="flex flex-col overflow-hidden relative h-screen w-screen">
       <header className="z-100 w-full min-h-20 h-20 flex bg-stone-950 comfortaa text-stone-100 items-center p-4 justify-between">
         <div className="flex gap-4 items-center text-3xl text-stone-100">
           {/* <img src={"../assets/chore-logo.svg"}/> */}
-          <Logo fill={"white"} width={"30px"} height={"30px"} />
+          <Logo fill={"white"} width={"35px"} height={"35px"} />
           Chore Community
+          <button
+            onClick={() => setOpen((p) => !p)}
+            className={`cursor-pointer ${
+              open ? "rotate-0" : "rotate-90"
+            } active:rotate-90 transition-all`}
+          >
+            <Bars3Icon className="w-6 h-6" />
+          </button>
         </div>
         <div></div>
         <div className="flex items-center justify-center gap-6">
           <div>
-            <StarIcon className="w-6 h-6" color="white" />
+            <MagnifyingGlassIcon className="w-6 h-6" color="white" />
           </div>
           <div className="bg-stone-800 px-4 py-2 border-3 border-stone-100 flex justify-center items-center text-xs">
             Log In
@@ -25,12 +38,28 @@ export default function MainPage() {
         </div>
       </header>
 
-      <div className="relative flex flex-col overflow-y-scroll overflow-x-hidden custom-scroll h-full w-full border-3 border-stone-100">
-        {avialablePages.map((Page, index) => (
-          <div key={index} className="relative flex flex-col min-w-full min-h-full max-w-full max-h-full">
-            <Page />
-          </div>
-        ))}
+      <div className="relative flex h-full w-full">
+        <div
+          className={`translate-y-10 transition-transform duration-500 ease-in-out ${
+            open ? "transform translate-x-0" : "transform -translate-x-full"
+          } w-64 bg-gray-800 z-10 fixed top-0 left-0 h-screen`}
+        >
+          <Sidebar />
+        </div>
+        <div
+          className={`w-full h-full flex flex-col overflow-x-hidden overflow-y-scroll custom-scroll border-3 border-stone-100 transition-all duration-500 ${
+            open ? "ml-64" : "ml-0"
+          }`}
+        >
+          {avialablePages.map((Page, index) => (
+            <div
+              key={index}
+              className="relative flex flex-col min-w-full min-h-full max-w-full max-h-full"
+            >
+              <Page />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
